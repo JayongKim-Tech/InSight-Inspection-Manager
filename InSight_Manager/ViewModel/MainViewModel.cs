5d6758ded31a81101ac0b70c892be52152e175e4
mwson <mwson@faplus.co.kr>
2025-12-26 15:23:23 -0600

    insight-Functiom

---
+++ InSight_Manager/ViewModel/MainViewModel.cs
@@ -0,0 +1,66 @@
+﻿using InSight_Inspection_Manager;
+using Insight_Manager.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Insight_Manager.ViewModel
+{
+    public class MainViewModel: ViewModelBase
+    {
+        ConnectModel model = new ConnectModel();
+
+        private string _ipAddress = "127.0.0.1";
+        private int port = 23;
+        private string _status = "Null";
+
+        public ICommand ConnectCommand { get; }
+
+        public string IpAddress
+        {
+            get => _ipAddress;
+            set
+            {
+                _ipAddress = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Status
+        {
+            get => _status;
+            set
+            {
+                _status = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void OnConnect(object obj)
+        {
+            if(model.Connect(IpAddress, port))
+            {
+                Status = "OK";
+            }
+            else
+            {
+                Status = "Fail";
+            }
+
+        }
+
+        public MainViewModel()
+        {
+            // ConnectCommand를 누르면 OnConnect 함수를 실행해라!
+            ConnectCommand = new RelayCommand(OnConnect);
+        }
+
+
+
+
+    }
+}
---
+++ InSight_Manager/ViewModel/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace InSight_Inspection_Manager
+{
+    // 이 클래스는 '화면에 값을 알리는 기능'만 전문적으로 담당합니다.
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        // 화면과 연결된 '종' (이벤트)
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // 종을 울리는 '행위' (함수)
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            // 나를 지켜보고 있는 화면이 있다면, "값이 바뀌었어!"라고 알려줌
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
